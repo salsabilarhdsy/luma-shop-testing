8e6efc0219fdedecc3f9df9e132bd0f76abcee1c
Salsa <salsabiladsy@gmail.com>
2024-01-25 10:29:48 +0700

    add visual testing example

---
+++ Scripts/Visual Testing/Script1706152991822.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://magento.softwaretestingboard.com/')
+
+WebUI.click(findTestObject('Object Repository/Visual Checkpoint/Page_Home Page/a_Sign In'))
+
+WebUI.setText(findTestObject('Object Repository/Visual Checkpoint/Page_Customer Login/input_Email_loginusername'), 'salsa@test.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Visual Checkpoint/Page_Customer Login/input_Password_loginpassword'), 
+    'R2dZ4hvJ2uiOEdyVG7iA0Q==')
+
+WebUI.click(findTestObject('Object Repository/Visual Checkpoint/Page_Customer Login/span_Sign In'))
+
+WebUI.takeScreenshotAsCheckpoint('login_success', FailureHandling.STOP_ON_FAILURE)
+
